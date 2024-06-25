0dc9ef6439ad9f720ce5763b466e0df7dd2bdb73
sanam453 <sanam.hamza9@gmail.com>
2024-06-25 10:04:46 +0430

    fixed .env file

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
+ 
+  
 }
 
 export const config = {
